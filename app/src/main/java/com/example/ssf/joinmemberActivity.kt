6f90cc222197e8107ba265cab5e676632a935e81
wjdwns <wjdwns3744@naver.com>
2021-02-23 22:18:42 +0900

    add timepicker

--- app/src/main/java/com/example/ssf/joinmemberActivity.kt
+++ app/src/main/java/com/example/ssf/joinmemberActivity.kt
@@ -3,14 +3,23 @@ package com.example.ssf
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
 import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_joinmember.*
+import java.util.*
 
 class joinmemberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_joinmember)
 
-        //val dialog = DatePickerDialog(this,listener,2000,0,1)
+
+        date_picker_actions.setOnClickListener {
+            val cal = Calendar.getInstance()
+            DatePickerDialog(this,DatePickerDialog.OnDateSetListener{
+                datePicker,y,m,d->
+            })
+        }
 
     }
 }
