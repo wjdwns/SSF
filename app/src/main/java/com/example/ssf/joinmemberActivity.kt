028fe4e882acad72280c0047b1d7f0e96baccd9a
wjdwns <wjdwns3744@naver.com>
2021-02-25 23:23:43 +0900

    datepicker 왜이리 어렵누

--- app/src/main/java/com/example/ssf/joinmemberActivity.kt
+++ app/src/main/java/com/example/ssf/joinmemberActivity.kt
@@ -12,20 +12,18 @@ class joinmemberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_joinmember)
-
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)+1
+        val day = cal.get(Calendar.DATE)
+        date_picker_actions.setText("$year-$month-$day")
         date_picker_actions.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val year = cal.get(Calendar.YEAR)
-            val month = cal.get(Calendar.MONTH)+1
-            val day = cal.get(Calendar.DATE)
-            date_picker_actions.setText("$year-$month-$day")
 
             var date_listener  = object : DatePickerDialog.OnDateSetListener {
                 override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
                     date_picker_actions.setText("$year-$month-$day")
                 }
             }
-
             var builder = DatePickerDialog(this, date_listener, year, month, day)
             builder.show()
 
