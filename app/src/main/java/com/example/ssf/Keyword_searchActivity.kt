3eaa4cca7935bd5572230dcbeea37cb399082604
wjdwns <wjdwns3744@naver.com>
2021-03-05 16:00:07 +0900

    버튼 수정 및 새로운 Activity 생성

---
+++ app/src/main/java/com/example/ssf/Keyword_searchActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ssf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Keyword_searchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keyword_search)
+    }
+}
---
+++ app/src/main/java/com/example/ssf/Search_SettingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ssf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Search_SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search__setting)
+    }
+}
